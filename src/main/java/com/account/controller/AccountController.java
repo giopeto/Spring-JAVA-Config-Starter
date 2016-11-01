97abc9000b7ffb5174a8e63a15342986548d2018
giopeto <georg3georgiev@gmail.com>
2016-11-01 19:44:25 +0200

    Spring JAVA Config Starter add spring security

--- src/main/java/com/account/controller/AccountController.java
+++ src/main/java/com/account/controller/AccountController.java
@@ -6,6 +6,9 @@ import com.account.service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.*;
 
@@ -34,13 +37,33 @@ public class AccountController {
 			headers="Accept=application/json",
 			produces="application/json"
 	)
-
 	public Account getCurrentAccount(Principal principal) {
-		Assert.notNull(principal);
-		return accountRepository.findByEmail(principal.getName());
-	}
 
-	@RequestMapping(
+		System.out.println("\n\n\n\n\nGet Current Account \n principal: " + principal  );
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+		System.out.println("\n\n\n\n\n ############ auth: " + auth  + "\n To string: " + auth.toString() + " NAME IS : " + auth.getName());
+		User asd = (User)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+
+		System.out.println("\n\n\n\n\n\n\n Usera: " + asd.toString());
+
+
+
+		//return accountRepository.findOneByEmail(auth.getName());
+		Assert.notNull(principal);
+		return accountRepository.findOneByEmail(principal.getName());
+	}
+	/*public Account getCurrentAccount(HttpServletRequest request) {
+		Principal principal = request.getUserPrincipal();
+		System.out.println("\n\n\n\n\nprincipal: " + principal  );
+
+		Assert.notNull(principal);
+		return accountRepository.findOneByEmail(principal.getName());
+	}*/
+
+
+	/*@RequestMapping(
 			value="/authenticate",
 			method = RequestMethod.POST,
 			headers="Accept=application/json",
@@ -48,7 +71,26 @@ public class AccountController {
 	)
 	public void authenticate(@RequestBody Account a) {
 		accountService.authenticate(a);
+	}*/
+
+	@RequestMapping(value = "account/{id}", method = RequestMethod.GET)
+	@ResponseStatus(value = HttpStatus.OK)
+	@ResponseBody
+	@Secured("ROLE_ADMIN")
+	public Account account(@PathVariable("id") String id) {
+		return accountRepository.findOne(id);
 	}
 
 
+	@RequestMapping(value = "/logOut", method = RequestMethod.GET)
+	public void logOut() {
+
+		Authentication auth = SecurityContextHolder.getContext()
+				.getAuthentication();
+		/*
+		 * System.out.println("Auth: " + auth); System.out.println("Auth 2: " +
+		 * auth.getCredentials()); System.out.println("Auth 3: " +
+		 * auth.getPrincipal());
+		 */
+	}
 }
--- src/main/java/com/account/service/AccountService.java
+++ src/main/java/com/account/service/AccountService.java
@@ -7,7 +7,10 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -16,6 +19,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.PostConstruct;
+import java.util.Collections;
 
 @Service
 @Scope(proxyMode = ScopedProxyMode.TARGET_CLASS)
@@ -45,12 +49,39 @@ public class AccountService implements UserDetailsService {
 		return account;
 	}
 
-	public void authenticate(Account account) {
+	/*public void authenticate(Account account) {
 		Authentication auth = new UsernamePasswordAuthenticationToken(account.getEmail(), account.getPassword());
 		SecurityContextHolder.getContext().setAuthentication(auth);
+
+		System.out.println("\n\n\n\n\n\n\n\nAuth : " + auth.isAuthenticated() + " " + auth.toString());
+
+	}*/
+
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		Account account = accountRepository.findOneByEmail(username);
+		if(account == null) {
+			throw new UsernameNotFoundException("user not found");
+		}
+		return createUser(account);
 	}
 
-	public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
-		return null;
+	public void signin(Account account) {
+		SecurityContextHolder.getContext().setAuthentication(authenticate(account));
 	}
+
+	private Authentication authenticate(Account account) {
+
+		Authentication auth = new  UsernamePasswordAuthenticationToken(createUser(account), null, Collections.singleton(createAuthority(account)));
+		System.out.println("\n\n\n\n\n\n\n\n Auth : " + auth.isAuthenticated() + " " + auth.toString());
+		return new UsernamePasswordAuthenticationToken(createUser(account), null, Collections.singleton(createAuthority(account)));
+	}
+
+	private User createUser(Account account) {
+		return new User(account.getEmail(), account.getPassword(), Collections.singleton(createAuthority(account)));
+	}
+
+	private GrantedAuthority createAuthority(Account account) {
+		return new SimpleGrantedAuthority(account.getRole());
+	}
+
 }
